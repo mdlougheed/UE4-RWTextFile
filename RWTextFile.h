92f307f3dc5f6359ec68e81889c776aa84e75c4e
mdlougheed <31193009+mdlougheed@users.noreply.github.com>
2018-01-07 15:45:28 -0800

    Updated source files with GNU license information

--- RWTextFile.h
+++ RWTextFile.h
@@ -1,4 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Unreal Engine 4 (UE4) Blueprint Class to read, write and append text files
+//
+// This code released under the terms and conditions of the GNU GPL3.0 License
+// by Mark D. Lougheed - 2018
+//
+// Original code attributed to Pally Qle:  https://drive.google.com/file/d/0B45Kp15-PhyOT1hyaDM0aUxyVEU/view
+// Original code YouTube tutorial: https://www.youtube.com/watch?v=wemG-YI2iag
 
 #pragma once
 
